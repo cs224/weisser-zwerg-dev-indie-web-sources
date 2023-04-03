7c0dd3badcf0f505b9ab8890c36988a497465e27
cs224 <cs224@weisser-zwerg.dev>
2023-04-03 04:52:19 +0200

    remove site10 test upgrade project.

--- site10/src/data/env.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-    console.log(`node_env: ${process.env.NODE_ENV}`);
-    return {
-        eleventy_env: process.env.ELEVENTY_ENV,
-            node_env:  process.env.NODE_ENV
-    };
-};
